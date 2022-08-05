4b192c827bd794e91a6d32f49da096660352f5a9
Girish Kumar Shakya <clairvoyant.1947@gmail.com>
2022-08-05 15:58:58 +0530

    Initial commit

---
+++ src/test/java/com/example/testingweb/SmokeTest.java
@@ -0,0 +1,41 @@
+package com.example.testingweb;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private HomeController controller;
+
+
+    @Test
+    public void contextLoads() throws Exception{
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void contextCheckNull() throws Exception{
+        assertThat(controller).isNull();
+    }
+
+    @Test
+    public void contextCheckNotNull() throws Exception{
+        assertThat(controller).isNotNull();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
